2ce00c7585e0ed6debef8c6d6174b4a7bd86506b
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2023-09-22 16:13:59 +0700

    Update

--- Semester-1/week-3/PRAK_DASPRO/ContohVariabe13.java
+++ Semester-1/week-3/PRAK_DASPRO/ContohVariabe13.java
@@ -2,11 +2,11 @@
  * ContohVariabe13
  */
 public class ContohVariabe13 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String hobby_Saya = "Bermain petak umpet";
         boolean isPandai_Saya = true;
         char kelamin_Saya = 'L';
-        byte umur_Saya  = 20;
+        byte umur_Saya = 20;
         double ipk_Saya = 3.24, tinggi_Saya = 1.78;
         System.out.println(hobby_Saya);
         System.out.println("Apakah pandai? " + isPandai_Saya);
