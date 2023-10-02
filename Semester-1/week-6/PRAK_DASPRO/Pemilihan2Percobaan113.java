0ca885b79a781bab2040a655e601561ba65c22d8
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2023-10-02 15:15:29 +0700

    Update: JobSheet 6 - Pertanyaan 4

--- Semester-1/week-6/PRAK_DASPRO/Pemilihan2Percobaan113.java
+++ Semester-1/week-6/PRAK_DASPRO/Pemilihan2Percobaan113.java
@@ -4,13 +4,16 @@ public class Pemilihan2Percobaan113 {
     public static void main(String[] args) {
         Scanner input13 = new Scanner(System.in);
         int tahun = input13.nextInt();
-        if ((tahun % 2) == 0) {
+        if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0)
+                System.out.println("Tahun Kabisat");
+            else if ((tahun % 400) == 0)
                 System.out.println("Tahun Kabisat");
             else
                 System.out.println("Bukan Tahun Kabisat");
         } else
             System.out.println("Bukan Tahun Kabisat");
+
         input13.close();
     }
 }
