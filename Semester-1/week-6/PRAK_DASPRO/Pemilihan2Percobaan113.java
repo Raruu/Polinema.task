39a56f6cdc5338cd7b82f5f967520ef5fd18f7f0
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2023-10-02 15:27:30 +0700

    Add: 1 line

--- Semester-1/week-6/PRAK_DASPRO/Pemilihan2Percobaan113.java
+++ Semester-1/week-6/PRAK_DASPRO/Pemilihan2Percobaan113.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Pemilihan2Percobaan113 {
     public static void main(String[] args) {
         Scanner input13 = new Scanner(System.in);
+        System.out.print("Masukkan tahun: ");
         int tahun = input13.nextInt();
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0)
