bebaeb8bbf81cdae74243352cb5406bd2e61cf56
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2023-10-12 17:53:47 +0700

    Pertanyaan 3

--- Semester-1/week-7/ForKelipatan13.java
+++ Semester-1/week-7/ForKelipatan13.java
@@ -4,16 +4,20 @@ public class ForKelipatan13 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int kelipatan, jumlah = 0, couter = 0;
+        float rataKelipatan = 0f;
         System.out.print("Masukkan bilangan kelipatan (1-9): ");
         kelipatan = scan.nextInt();
         for (int i = 1; i <= 50; i++) {
             if (i % kelipatan == 0) {
                 jumlah += i;
                 couter++;
+                rataKelipatan++;
             }
         }
+        rataKelipatan = jumlah / rataKelipatan;
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, couter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %f\n", kelipatan, rataKelipatan);
         scan.close();
     }
 }
