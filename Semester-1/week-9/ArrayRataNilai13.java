d77864f9e39f4a1ab5437772d97907d5c9bc0563
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2023-10-24 07:36:18 +0700

    Update: Test3-Pertanyaan

--- Semester-1/week-9/ArrayRataNilai13.java
+++ Semester-1/week-9/ArrayRataNilai13.java
@@ -3,17 +3,27 @@ import java.util.Scanner;
 public class ArrayRataNilai13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jumMahasiswa = sc.nextInt();
+        int[] nilaiMhs = new int[jumMahasiswa];
         double total = 0;
-        double rata2;
+        double rata2, rata2_tidakLulus = 0;
+        int countLulus = 0;
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
+            if (nilaiMhs[i] > 70) {
+                countLulus++;
+            } else
+                rata2_tidakLulus += nilaiMhs[i];
             total += nilaiMhs[i];
         }
         rata2 = total / nilaiMhs.length;
-        System.out.println("Rata-rate nilai = " + rata2);
+        rata2_tidakLulus /= (nilaiMhs.length - countLulus);
+        System.out.println("Rata-rate nilai lulus= " + rata2);
+        System.out.println("Rata-rate nilai tidak lulus= " + rata2_tidakLulus);
+        sc.close();
     }
 }
