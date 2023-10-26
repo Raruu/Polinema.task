aefb1ed402979a27e6aa5ae5127ff7b18fd8efcc
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2023-10-27 06:10:06 +0700

    Update: Test4-Pertanyaan

--- Semester-1/week-9/LinearSearch13.java
+++ Semester-1/week-9/LinearSearch13.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
+
 public class LinearSearch13 {
     public static void main(String[] args) {
-        int[] arrayInt = { 34, 18, 26, 48, 72, 20, 56, 63 };
-        int key = 20;
+        Scanner scInput = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen array: ");
+        int elemen = scInput.nextInt();
+        int[] arrayInt = new int[elemen];
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke-" + i + " : ");
+            arrayInt[i] = scInput.nextInt();
+        }
+        System.out.print("Masukkan key yang ingin dicari: ");
+        int key = scInput.nextInt();
         int hasil = 0;
-
+        Boolean keyFound = false;
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
+                keyFound = true;
                 break;
             }
         }
-        System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+        if (keyFound)
+            System.out.println("Key ada di posisi indeks ke-" + hasil);
+        else
+            System.out.println("Key tidak ditemukan");
+        scInput.close();
     }
 }
