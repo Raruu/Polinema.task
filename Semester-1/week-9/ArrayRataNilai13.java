a775b370e10e760263b4f2d3767be36719afb0de
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2023-10-26 10:29:25 +0700

    Fix: rata

--- Semester-1/week-9/ArrayRataNilai13.java
+++ Semester-1/week-9/ArrayRataNilai13.java
@@ -6,8 +6,7 @@ public class ArrayRataNilai13 {
         System.out.print("Masukkan jumlah mahasiswa : ");
         int jumMahasiswa = sc.nextInt();
         int[] nilaiMhs = new int[jumMahasiswa];
-        double total = 0;
-        double rata2, rata2_tidakLulus = 0;
+        double rata2 = 0, rata2_tidakLulus = 0;
         int countLulus = 0;
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
@@ -15,12 +14,12 @@ public class ArrayRataNilai13 {
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
             if (nilaiMhs[i] > 70) {
+                rata2 += nilaiMhs[i];
                 countLulus++;
             } else
                 rata2_tidakLulus += nilaiMhs[i];
-            total += nilaiMhs[i];
         }
-        rata2 = total / nilaiMhs.length;
+        rata2 /= countLulus;
         rata2_tidakLulus /= (nilaiMhs.length - countLulus);
         System.out.println("Rata-rate nilai lulus= " + rata2);
         System.out.println("Rata-rate nilai tidak lulus= " + rata2_tidakLulus);
