2faf94e66ed96100845a8aa99a302d22cee08760
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2023-11-06 17:35:17 +0700

    init: Tugas Individu(1-3)

---
+++ Semester-1/week-11/TugasIndividuToKelompok/pertanyaan2.java
@@ -0,0 +1,18 @@
+package TugasIndividuToKelompok;
+
+import java.util.Scanner;
+
+public class pertanyaan2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan nilai N: ");
+        int N = scanner.nextInt();
+        scanner.close();
+        for (int i = N; i > 0; i--) {
+            for (int j = N - i; j < N; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Semester-1/week-11/TugasIndividuToKelompok/pertanyaan3.java
@@ -0,0 +1,23 @@
+package TugasIndividuToKelompok;
+
+import java.util.Scanner;
+
+public class pertanyaan3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan nilai N: ");
+        int N = scanner.nextInt();
+        scanner.close();
+        for (int i = 0; i < N; i++) {
+            System.out.print(N);
+            for (int j = 1; j < N - 1; j++) {
+                if (i == 0 || i == N - 1)
+                    System.out.print(N);
+                else
+                    System.out.print(' ');
+            }
+            System.out.print(N);
+            System.out.println();
+        }
+    }
+}
